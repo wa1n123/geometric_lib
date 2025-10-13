8372025d9323e602fef3308ad02c1756bce355a0
Alexej Medvedenko <medvedenko.al2007@gmail.com>
2025-10-14 01:17:20 +0300

    Добавлены комментарии для PY файлов

--- circle.py
+++ circle.py
@@ -2,9 +2,26 @@ import math
 
 
 def area(r):
+    """
+    Вычисляет площадь круга.
+    
+    Parameters:
+        r (float): Радиус круга
+        
+    Returns:
+        float: Площадь круга по формуле π * r^2
+    """
     return math.pi * r * r
 
 
 def perimeter(r):
+    """
+    Вычисляет длину окружности (периметр круга).
+    
+    Parameters:
+        r (float): Радиус круга
+        
+    Returns:
+        float: Длина окружности по формуле 2 * π * r
+    """
     return 2 * math.pi * r
-
--- square.py
+++ square.py
@@ -1,7 +1,25 @@
 
 def area(a):
+    """
+    Вычисляет площадь квадрата.
+    
+    Parameters:
+        a (float): Длина стороны квадрата
+        
+    Returns:
+        float: Площадь квадрата по формуле a * a
+    """
     return a * a
 
 
 def perimeter(a):
+    """
+    Вычисляет периметр квадрата.
+    
+    Parameters:
+        a (float): Длина стороны квадрата
+        
+    Returns:
+        float: Периметр квадрата по формуле 4 * a
+    """
     return 4 * a
